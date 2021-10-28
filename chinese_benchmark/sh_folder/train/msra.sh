362c4eb80e319e7d2ea10006a57567b9a625e638
Yixuan Su <syxistheone@gmail.com>
2021-10-28 12:33:16 +0100

    Update msra.sh

--- chinese_benchmark/sh_folder/train/msra.sh
+++ chinese_benchmark/sh_folder/train/msra.sh
@@ -9,7 +9,7 @@ CUDA_VISIBLE_DEVICES=6,7 python  ../../train.py\
     --batch_size_per_gpu 64\
     --number_of_gpu 2\
     --gradient_accumulation_steps 1\
-    --total_epochs 100\
+    --total_epochs 40\
     --number_of_runs 5\
     --save_path_prefix ../../ckpt/msra/
 
