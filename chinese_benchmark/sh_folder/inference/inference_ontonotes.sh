c044e396304dc54c15e8befadb5c167135346f76
Yixuan Su <syxistheone@gmail.com>
2021-11-03 14:18:44 +0000

    Update inference_ontonotes.sh

--- chinese_benchmark/sh_folder/inference/inference_ontonotes.sh
+++ chinese_benchmark/sh_folder/inference/inference_ontonotes.sh
@@ -1,5 +1,5 @@
 CUDA_VISIBLE_DEVICES=0 python  ../../inference.py\
-    --model_name cambridgeltl/clbert-base-chinese\
+    --model_name cambridgeltl/tacl-bert-base-chinese\
     --saved_ckpt_path ../../pretrained_ckpt/ontonotes/ontonotes_ckpt\
     --train_path ../../../benchmark_data/NER/OntoNote4NER/OntoNote4NER.train.char.txt\
     --dev_path ../../../benchmark_data/NER/OntoNote4NER/OntoNote4NER.dev.char.txt\
