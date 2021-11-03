5d4244a8006e06b48a015d1a53849736094a3253
Yixuan Su <syxistheone@gmail.com>
2021-11-03 14:18:54 +0000

    Update inference_pku.sh

--- chinese_benchmark/sh_folder/inference/inference_pku.sh
+++ chinese_benchmark/sh_folder/inference/inference_pku.sh
@@ -1,5 +1,5 @@
 CUDA_VISIBLE_DEVICES=0 python  ../../inference.py\
-    --model_name cambridgeltl/clbert-base-chinese\
+    --model_name cambridgeltl/tacl-bert-base-chinese\
     --saved_ckpt_path ../../pretrained_ckpt/pku_cws/pku_ckpt\
     --train_path ../../../benchmark_data/CWS_data/PKU/pku_train_all_processed.txt\
     --dev_path ../../../benchmark_data/CWS_data/PKU/pku_dev_processed.txt\
