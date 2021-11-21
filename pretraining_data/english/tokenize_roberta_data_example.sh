ce2189ccedd69c46fc6b521d67217cc839861b20
Yixuan Su <syxistheone@gmail.com>
2021-11-21 10:15:03 +0000

    Delete tokenize_roberta_data_example.sh

--- pretraining_data/english/tokenize_roberta_data_example.sh
+++
@@ -1,5 +0,0 @@
-python tokenize_data.py\
-    --model_name roberta-base\
-    --raw_data_path ./english_wiki_20k_lines.txt\
-    --output_dir ./roberta/\
-    --output_name roberta_tokenized_wiki_example.txt
